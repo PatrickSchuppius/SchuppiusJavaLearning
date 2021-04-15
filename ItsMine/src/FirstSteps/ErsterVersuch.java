19a68162d2b0c15ebdd56f8ee952f6f00e6136a4
schupwi <schupwi@hlag.com>
2021-04-15 17:13:46 +0200

    neue Klasse ErsterVersuch

---
+++ ItsMine/src/FirstSteps/ErsterVersuch.java
@@ -0,0 +1,12 @@
+package FirstSteps;
+
+
+public class ErsterVersuch {
+
+	public static void main(String[] inputArgs) {
+		int unsereNummer = 5;
+		System.out.println(unsereNummer);
+		System.out.println("Juhuu");
+	}
+
+}
