d6c52460e309ca2f5db79af7a13443a5e7798e47
schupwi <schupwi@hlag.com>
2021-04-29 15:57:49 +0200

    und rein damit

--- ItsMine/src/FirstSteps/ErsterVersuch.java
+++ ItsMine/src/FirstSteps/ErsterVersuch.java
@@ -4,8 +4,12 @@ package FirstSteps;
 public class ErsterVersuch {
 
 	public static void main(String[] inputArgs) {
-		int unsereNummer = 5;
-		System.out.println(unsereNummer);
+		int myFirstNumber = 5;
+		int mySecondNumber = 12;
+		int myThirdNumber = 6;
+		int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+		int uupps;
+		System.out.println(myTotal);
 		System.out.println("Juhuu");
 	}
 
---
+++ ItsMine/src/FirstSteps/NumberCrunching.java
@@ -0,0 +1,19 @@
+package FirstSteps;
+
+
+public class NumberCrunching {
+
+	public static void main(String[] args) {
+
+		// byte myByte = Byte.MAX_VALUE;
+		// short myShort = Short.MAX_VALUE;
+		// int myInt = Integer.MAX_VALUE;
+		// long myLong = 50_000 + 10 * (myByte + myShort + myInt);
+
+		double myPounds = 37;
+		double myKilos = myPounds * 0.45359237;
+		System.out.println("Die Kilos sind: " + myKilos);
+
+	}
+
+}
