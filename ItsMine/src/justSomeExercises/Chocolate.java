fd052de39342ad84c45c45824db007ce51f16b93
schupwi <schupwi@hlag.com>
2021-12-09 15:09:09 +0100

    some exercise

---
+++ ItsMine/src/justSomeExercises/Chocolate.java
@@ -0,0 +1,15 @@
+package justSomeExercises;
+
+
+public class Chocolate extends Food {
+
+	public Chocolate(String name) {
+		super(name);
+	}
+
+	@Override
+	public int getCalories() {
+		return 53;
+	}
+
+}
---
+++ ItsMine/src/justSomeExercises/Food.java
@@ -0,0 +1,23 @@
+package justSomeExercises;
+
+
+public abstract class Food {
+
+	private String name;
+
+	public Food(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public abstract int getCalories();
+
+}
+
